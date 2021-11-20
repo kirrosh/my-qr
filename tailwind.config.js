c883fd43d7e0b39076e401f6b5a856a71d6a76df
Kirill Koshelev <olegartor7@gmail.com>
2021-11-20 11:21:39 +0300

    [core] tailwind deploy fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const tailwindMobile = require('tailwind-mobile/config');
 
 // wrap config with tailwindMobile config
 module.exports = tailwindMobile({
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './features/**/*.{js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
