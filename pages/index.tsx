a0442dba833744057ede0307ce0118766721b877
Kirill Koshelev <olegartor7@gmail.com>
2021-11-21 23:53:41 +0300

    [text] added text into add button

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,6 +43,7 @@ const Home: NextPage = () => {
             <Fab
                 className="fixed left-1/2 bottom-4-safe transform -translate-x-1/2 z-20"
                 icon={<MdAdd />}
+                text="Добавить QR код"
                 onClick={() => setPopupOpened(true)}
             />
         </Page>
