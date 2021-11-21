a0c0e90a53168bea9ff4aca4ad1fa22c14a3f0b7
Kirill Koshelev <olegartor7@gmail.com>
2021-11-21 20:40:58 +0300

    [next] fix config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
     reactStrictMode: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/],
     pwa: {
         dest: 'public'
     }
