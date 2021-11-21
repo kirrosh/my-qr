ccc14e8f88749dcabf599b7d99e2c232454607d9
Kirill Koshelev <olegartor7@gmail.com>
2021-11-21 20:54:16 +0300

    [next] config

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
     reactStrictMode: true,
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest\.json$/],
     pwa: {
-        dest: 'public'
+        dest: 'public',
+        runtimeCaching,
+        buildExcludes: [/middleware-manifest\.json$/]
     }
     // other next config
 });
