ef46ff2293b70a52b58aa3e122cd0b37afb108cf
Kirill Koshelev <olegartor7@gmail.com>
2021-11-22 10:41:40 +0300

    [text] added rus metatext

--- features/MetaData.tsx
+++ features/MetaData.tsx
@@ -8,15 +8,15 @@ const MetaData = () => {
             <meta name="description" content="Save your QR" />
             <link rel="icon" href="/favicon.ico" />
 
-            <meta name="application-name" content="PWA App" />
+            <meta name="application-name" content="My QR" />
             <meta name="apple-mobile-web-app-capable" content="yes" />
             <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-            <meta name="apple-mobile-web-app-title" content="PWA App" />
-            <meta name="description" content="Best PWA App in the world" />
+            <meta name="apple-mobile-web-app-title" content="My QR" />
+            <meta name="description" content="Быстрый доступ к QR коду" />
             <meta name="format-detection" content="telephone=no" />
             <meta name="mobile-web-app-capable" content="yes" />
             <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-            <meta name="msapplication-TileColor" content="#2B5797" />
+            <meta name="msapplication-TileColor" content="#cfdef5" />
             <meta name="msapplication-tap-highlight" content="no" />
             <meta name="theme-color" content="#000000" />
 
